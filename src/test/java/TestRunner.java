0aacdacf2b24b29cc20cf514d47d0bf78dda7f29
aga <ag.rakowska@gmail.com>
2020-11-04 21:08:46 +0100

    Run test parallel

--- src/test/java/TestRunner.java
+++ src/test/java/TestRunner.java
@@ -1,9 +1,15 @@
-import com.intuit.karate.junit5.Karate;
+import com.intuit.karate.Results;
+import com.intuit.karate.Runner;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 class TestRunner {
 
-    @Karate.Test
-    Karate testSample() {
-        return Karate.run("classpath:sample.feature");
+    @Test
+    public void testParallel() {
+        Results results = Runner.path("classpath:sample.feature").tags("~@ignore").parallel(5);
+        assertEquals(0, results.getFailCount(), results.getErrorMessages());
     }
 }
